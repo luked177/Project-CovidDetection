e3701d21338a1544cab09bda616c777ddd24bf39
luked177 <43680876+luked177@users.noreply.github.com>
2021-04-13 15:00:14 +0100

    Update Libraries

--- segment.py
+++ segment.py
@@ -1,12 +1,5 @@
-import tkinter as tk
-from tkinter import messagebox #messagebox
-from tkinter import simpledialog
-from tkinter import filedialog
-import datetime
-from pandas import DataFrame
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+# Import Libraries
+
 
 def originalImages():
     print("Read Nifti Images")
